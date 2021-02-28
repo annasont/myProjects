056e70f969f023238441af2e2c0f850def3dbb17
Anna Sontowska <aniasontowska@gmail.com>
2021-02-28 17:36:35 +0100

    Added documentation to checkDpi.py; added program downloadPKD.py

--- checkDpi.py
+++ checkDpi.py
@@ -1,23 +1,34 @@
+'''checkDpi.py checks if images in given location are big enough for print (or other purposes).'''
+
 import os, shutil
 from PIL import Image
 
 def readWidthAndHeight(path, filename):
+    # Checks images with and height in pixels
     imagePath = os.path.join(path, filename)
     image = Image.open(imagePath)
     sourceImageWidth, sourceImageHeight = image.size
     return sourceImageWidth, sourceImageHeight
 
 def checkFiles(path, widthInPrint, heightInPrint, dpi):
+    # path: location of images you want to check
+    # widthInPrint: expected width of an image in printed material (in centimeters)
+    # heightInPrint: expected height of an image in printed material (in centimeters)
+    # dpi: expected value of dpi (use 300 dpi for printed matierials)
     okForPrint = []
     notOkForPrint = []  
     for filename in os.listdir(path):
+        # checking only files in .jpg and .png format:
         if filename.endswith('.jpg') or filename.endswith('.JPG') or filename.endswith('.png') or filename.endswith('.PNG'):
             sourceImageWidth, sourceImageHeight = readWidthAndHeight(path, filename)
+            # calculating minimum width and height (in pixels) for given size of image in printed material: 
             minWidth = round(widthInPrint / 2.54 * dpi)
             minHeight = round(heightInPrint / 2.54 * dpi)
+            # checking if image file is big enough to print it in given quality and given size:
             if sourceImageWidth >= minWidth and sourceImageHeight >= minHeight:
                 okForPrint.append(filename)
             else:
+                # if image not big enough, calculating maximum size of an image in print, in order to maintain given quality.
                 maxWidth = round(sourceImageWidth / dpi * 2.54)
                 maxHeight = round(sourceImageHeight / dpi * 2.54)
                 text = '%s: max width of %s cm; max height of %s cm' % (filename, maxWidth, maxHeight)
@@ -32,6 +43,7 @@ def checkFiles(path, widthInPrint, heightInPrint, dpi):
     return okForPrint
 
 def moveFilesOkForPrint():
+    # Creates folder "OkForPrint" and moves files that match requirements to this folder 
     okForPrint = checkFiles(path, widthInPrint, heightInPrint, dpi)
     newFolder = os.path.join(path, "OkForPrint")
     os.makedirs(newFolder, exist_ok=True)
@@ -43,12 +55,10 @@ def moveFilesOkForPrint():
 
     print('\nFiles big enough for print moved to the new folder "OkForPrint".')
 
-path = r'C:\Users\annaso\Desktop\aktualne\test\focie'
-path2 = r''
-widthInPrint = 10
-heightInPrint = 10
-dpi = 300
+# path = r'C:\Users\annaso\Desktop\aktualne\test\focie'
+# widthInPrint = 10
+# heightInPrint = 10
+# dpi = 300
 
-checkFiles(path, widthInPrint, heightInPrint, dpi)
-
-#moveFilesOkForPrint()
+# checkFiles(path, widthInPrint, heightInPrint, dpi)
+# moveFilesOkForPrint()
--- downloadPKD.py
+++ downloadPKD.py
@@ -1,10 +1,82 @@
-'''downloadPKD.py'''
-# Mini wersja: sciaga excela 
-# do odpowiedniego folderu przenosi
-# Bardziej rozbudowana: w poniedzialek sciaga z soboty, niedzieli i poniedzialku.
-
+'''downloadPKD.py: on Tuesdays to Fridays downloads current PKD reports as Excel spreadsheets and moves them to the chosen folder.
+On Mondays downloads in addition reports from Saturday and Sunday.'''
 
 from selenium import webdriver
-browser = webdriver.Firefox()
+from selenium.webdriver.common.keys import Keys
+import time, pyautogui, os, shutil, datetime
 
-browser.get('https://www.pse.pl/dane-systemowe/plany-pracy-kse/plan-koordynacyjny-dobowy-pkd/wielkosci-podstawowe')
+def openPage():
+    # Opens webbrowser on the webpage with PKD reports
+    browser = webdriver.Firefox()
+    browser.get('https://www.pse.pl/dane-systemowe/plany-pracy-kse/plan-koordynacyjny-dobowy-pkd/wielkosci-podstawowe')
+    return browser
+
+def downloadExcel(browser):
+    # Chooses Excel-format and clicks download
+    try:
+        reportInExcel = browser.find_element_by_link_text('Excel')
+        reportInExcel.click()
+    except:
+        print('not found')
+
+def findWeekendExcel(browser, day):
+    # Changes date
+    try:
+        parameters = browser.find_element_by_link_text('Parametry')
+        parameters.click()
+        input = browser.find_element_by_id('__param__VisioPortlet_WAR_visioneoportlet_INSTANCE_oyPkFYlXjRRu_data')
+        input.clear()
+        input.send_keys(day.strftime(r'%Y-%m-%d'))
+        confirm = browser.find_element_by_xpath('//button[normalize-space()="Zatwierdź"]')
+        confirm.click()
+    except:
+        print('not found')
+
+def saveFile(ax, ay, bx, by):
+    # When using linux window with options “Open with” and “Save file” shows up. 
+    # Function uses pyautogui to choose the option “Save file” and “OK”. Adjust coordinates to your screen.
+    for i in range(5):
+        time.sleep(1)
+    pyautogui.click(ax, ay)
+    time.sleep(2)
+    pyautogui.click(bx, by)
+
+def moveToFolder(sourceFolder, destinationFolder):
+    # Moves files to given destination
+    for filename in os.listdir(sourceFolder):
+        if filename.startswith('PL_PKD'):
+            source = os.path.join(sourceFolder, filename)
+            destination = os.path.join(destinationFolder, filename)
+            if not filename in os.listdir(destinationFolder):
+                shutil.move(source, destination)
+                print('File %s moved to destination folder.' % filename )
+            else:
+                print('Did not move file %s. File was already in the destination folder.' % filename)
+
+
+if datetime.datetime.today().weekday() != 0:
+    browser = openPage()
+    downloadExcel(browser)
+    saveFile(506, 470, 910, 540)
+    moveToFolder('/home/ania/Downloads', '/home/ania/Documents/PKD')
+else:
+    browser = openPage()
+    downloadExcel(browser)
+    saveFile(506, 470, 910, 540)
+    moveToFolder('/home/ania/Downloads', '/home/ania/Documents/PKD')
+
+    Monday = datetime.datetime.now()
+    oneDay = datetime.timedelta(days=1)
+    Sunday = Monday - oneDay
+    Saturday = Sunday - oneDay
+
+    findWeekendExcel(browser, Saturday)
+    downloadExcel(browser)
+    saveFile(506, 470, 910, 540)
+    moveToFolder('/home/ania/Downloads', '/home/ania/Documents/PKD')
+
+    findWeekendExcel(browser, Sunday)
+    downloadExcel(browser)
+    saveFile(506, 470, 910, 540)
+    moveToFolder('/home/ania/Downloads', '/home/ania/Documents/PKD')
+
